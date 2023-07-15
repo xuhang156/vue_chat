b9586d8332043370a6731a12bd05eefc181ac840
徐航 | Hang Xu <xu.hang@mech-mind.net>
2023-07-15 18:07:28 +0800

    feat：设置吸顶效果

---
+++ src/components/sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="sidebar" :style="sidbarStyle">
+        <p>侧边栏</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "sidebar",
+        data() {
+            return {
+            }
+        },
+        computed:{
+            sidbarStyle(){
+                return {
+                    margin: '0px',
+                    position: 'relative',
+                    width:'15%',
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.sidebar {
+    background-color:blueviolet;
+}
+</style>
